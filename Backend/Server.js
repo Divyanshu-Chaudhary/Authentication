a91916d7eb39e972746b72f1bb228aa8874849ea
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-01-08 11:30:13 +0530

    Bind server to all network interfaces

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,6 +19,6 @@ const PORT = process.env.PORT || 3000;
 //     res.send('Authentication Backend Server is running');
 // });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
